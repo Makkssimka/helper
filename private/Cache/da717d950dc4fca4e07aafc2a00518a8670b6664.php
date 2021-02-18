77cf4485c9c52dbe141758e3a161a7538aee60a4
Makkssimka <swoisait@gmail.com>
2021-02-18 10:07:40 +0300

    Добавил favicon

--- private/Cache/da717d950dc4fca4e07aafc2a00518a8670b6664.php
+++ private/Cache/da717d950dc4fca4e07aafc2a00518a8670b6664.php
@@ -5,10 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo e($title); ?></title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.16/dist/css/uikit.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/styles/style.css">
-    <title><?php echo e($title); ?></title>
+
 </head>
 <body>
     <?php echo $__env->make('menu_top', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
