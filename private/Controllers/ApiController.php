07bcb1d19036c931de3206b64dd28482747eee49
Makkssimka <swoisait@gmail.com>
2021-03-15 23:26:29 +0300

    тестирование загрузки прайса - часть 4

--- private/Controllers/ApiController.php
+++ private/Controllers/ApiController.php
@@ -10,17 +10,19 @@ class ApiController
 {
     function loadPrice() {
         $mode = Request::get('mode');
-        $string = date('d-m-Y H:i', time())."Загрузка данных с сайта".PHP_EOL;
+        $string = date('d-m-Y H:i', time())." Загрузка данных с сайта".PHP_EOL;
         file_put_contents('data/load.log', $string, FILE_APPEND);
         if ($mode == 'checkauth') {
             $val = md5(time());
             setcookie('hash', $val);
             echo "success\nhash\n$val";
+        } elseif ($mode == 'init') {
+            echo "zip=yes\nfile_limit=52428800";
         }
     }
 
     function showPrice() {
         $result = file_get_contents('data/load.log');
-        var_dump($result);
+        echo $result;
     }
 }
